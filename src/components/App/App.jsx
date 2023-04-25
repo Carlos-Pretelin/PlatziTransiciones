10680ede5af04e4a8047fd3088a92ecc73b9c7b9
Carlos Pretelin <carlosppretelin@gmail.com>
2023-04-25 06:24:03 -0600

    Pseudo Classes

---
+++ src/components/App/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import "./App.scss"
+
+const App = () => {
+  return (
+    <div className='App'>
+      <p>Hello World</p>
+      <a href="" className='firstName'>Carlos</a>
+      <a href="">Luis</a>
+    </div>
+  )
+}
+
+export default App
